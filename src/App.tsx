ab63fabad7ae08969d4467f080011207ff7ba8c5
Steffany-Martins <steffany@zee-dog.com>
2022-10-17 23:52:05 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl">Hello World</h1>
+      <button className=" bg-cyan-300">Enviar</button>
+    </>
+  )
+}
+
